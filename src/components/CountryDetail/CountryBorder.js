fabb6835b06e72bcd7aed64c09f22cdbdbac923a
divine <divineobi07@gmail.com>
2022-06-15 22:06:45 +0100

    add Country Border component

---
+++ src/components/CountryDetail/CountryBorder.js
@@ -0,0 +1,17 @@
+import "./CountryBorder.css";
+
+const CountryBorder = () => {
+  return (
+    <section className="country_border_countries">
+      <h2>Border Countries</h2>
+      <div className="country_border_box">
+        <span>France</span>
+        <span>Germany</span>
+        <span>Netherlands</span>
+      </div>
+    </section>
+  );
+};
+
+
+export default CountryBorder;
